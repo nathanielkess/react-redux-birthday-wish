a308c0f3c465efd4c75bee1bb76c949e2395835c
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2017-04-17 08:23:07 -0400

    birthday cake styles

--- src/components/text-input.js
+++ src/components/text-input.js
@@ -3,6 +3,15 @@ import React from 'react';
 const TextInput = ({ onNewVaue }) =>
 <div>
   <textarea onKeyUp={(event) => onNewVaue(event.target.value)} />
+  <div className="cake">
+    <div className="topping-1"></div>
+    <div className="topping-1"></div>
+    <div className="topping-1"></div>
+    <div className="layer"></div>
+    <div className="layer"></div>
+    <div className="layer"></div>
+    <div className="plate"></div>
+  </div>
 </div>
 
 export default TextInput;
