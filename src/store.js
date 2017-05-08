052371dd623c21ee962719dd8813f648fbbadaf7
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2017-05-07 23:30:31 -0400

    add reducers to the store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import { count } from './features/make-a-wish/make-a-wish.reducers';
+import { minLetterCount, isWishMade } from './features/grant-wish/grant-wish.reducers';
 
 const logger = createLogger({
   collapsed: true,
 });
 
 const rootReducer = combineReducers({
-  count
+  count,
+  minLetterCount,
+  isWishMade,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
