1a9f4d3be583e6c47eecdb7aebdd090318659ef3
Nathaniel Kessler <nathaniel@nathanielkessler.com>
2017-05-10 10:13:58 -0400

    solution - 05

--- src/App.js
+++ src/App.js
@@ -9,35 +9,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      wishCount: 0,
-      minLetterCount: 0,
-      isWishGranted: false,
-      isPaused: false,
       isTimeRemaining: true,
     }
-    this.handleWishCountChange = this.handleWishCountChange.bind(this);
-    this.handleLetterCountChange = this.handleLetterCountChange.bind(this);
-    this.handleWishStatus = this.handleWishStatus.bind(this);
     this.handleTimerComplete = this.handleTimerComplete.bind(this);
-  }
-
-  handleWishCountChange(count){
-    this.setState({
-      wishCount: count,
-    });
-  }
-
-  handleLetterCountChange(count) {
-    this.setState({
-      minLetterCount: count,
-    });
-  }
-
-  handleWishStatus(isWishGranted){
-    this.setState({
-      isPaused: true,
-      isWishGranted
-    });
   }
 
   handleTimerComplete(){
@@ -49,15 +23,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <MakeAWish 
-            onWishCountChange={this.handleWishCountChange} 
-            minLetters={this.state.minLetterCount} 
-          />
+          <MakeAWish />
           <GrantWish 
             isTimeRemaining={this.state.isTimeRemaining}
-            wishCount={this.state.wishCount} 
-            onWishStatus={this.handleWishStatus}
-            onMinLetterCount={this.handleLetterCountChange} 
           />
           <Timer 
             pause={this.state.isPaused}
